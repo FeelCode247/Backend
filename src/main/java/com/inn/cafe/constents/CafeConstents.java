94c274d2f9ff597fa35614265c1836ac23ec77e0
FeelCode247 <chaukatenarayan@gmail.com>
2024-03-12 13:17:30 +0530

    first commit

---
+++ src/main/java/com/inn/cafe/constents/CafeConstents.java
@@ -0,0 +1,12 @@
+package com.inn.cafe.constents;
+
+public class CafeConstents {
+
+    public static final String SOMETHING_WENT_WRONG= "Somthing Went Wrong";
+
+    public static final String INVALID_DATA="Invalid data";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized Access.";
+
+    public static final String STORE_LOCATION = "C:\\Users\\Tanaji\\Downloads\\AllfilesJava";
+}
